a96967c703996b1723a2d3231fe764f3c4d9e038
John Axe <jigarkhwar88@gmail.com>
2024-05-09 08:55:25 -0300

    chore: maven support

--- src/main/scala/org/galaxio/gatling/amqp/request/AmqpDslBuilderBase.scala
+++ src/main/scala/org/galaxio/gatling/amqp/request/AmqpDslBuilderBase.scala
@@ -4,7 +4,8 @@ import io.gatling.core.config.GatlingConfiguration
 import io.gatling.core.session._
 
 case class AmqpDslBuilderBase(requestName: Expression[String]) {
-  def publish(implicit configuration: GatlingConfiguration)      = PublishDslBuilderExchange(requestName, configuration)
-  def requestReply(implicit configuration: GatlingConfiguration) =
+  def publish(implicit configuration: GatlingConfiguration): PublishDslBuilderExchange           =
+    PublishDslBuilderExchange(requestName, configuration)
+  def requestReply(implicit configuration: GatlingConfiguration): RequestReplyDslBuilderExchange =
     RequestReplyDslBuilderExchange(requestName, configuration)
 }
